bbe006c947101899ed21056531451c7e79b2ba2b
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2022-07-22 11:51:08 +0200

    Update

--- dotfiles/config/nvim/lua/user/options.lua
+++ dotfiles/config/nvim/lua/user/options.lua
@@ -15,14 +15,14 @@ vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.swapfile = false
 vim.opt.timeoutlen = 1000
- -- vim.opt.termguicolors = true
+-- vim.opt.termguicolors = true
 vim.opt.undofile = true
 vim.opt.updatetime = 300
 vim.opt.writebackup = false
 vim.opt.expandtab = false
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
-vim.opt.backspace = { "indent","eol","start" }
+vim.opt.backspace = { "indent", "eol", "start" }
 vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -31,5 +31,5 @@ vim.opt.signcolumn = "yes"
 vim.opt.wrap = false
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
-vim.opt.shortmess:append "c"
-vim.cmd [[set iskeyword+=-]]
+vim.opt.shortmess:append("c")
+vim.cmd([[set iskeyword+=-]])
